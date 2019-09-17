99384d47f3ffe6e27db44e32d5507a8f51ce7ea5
pochito427 <djalfo18@gmail.com>
2019-09-17 17:12:15 +0000

    Commit changes for static pages: basics, ruby and git

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  get 'basics', to: 'static_pages#basics', as: :basics
+  get 'ruby', to: 'static_pages#ruby', as: :ruby
+  get 'git', to: 'static_pages#git', as: :git
+  root 'static_pages#home'
 end
